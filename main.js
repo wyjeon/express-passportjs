93a302b8b0cf55aacaaaf6618ecf9630a3ad80d5
wyjeon <wyjeon.info@gmail.com>
2021-02-01 14:12:18 +0900

    2. passport 설치

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var compression = require('compression');
-var helmet = require('helmet')
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var compression = require("compression");
+var helmet = require("helmet");
 app.use(helmet());
-var session = require('express-session')
-var FileStore = require('session-file-store')(session)
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-app.use(session({
-  secret: 'asadlfkj!@#!@#dfgasdg',
-  resave: false,
-  saveUninitialized: true,
-  store:new FileStore()
-}))
+app.use(
+  session({
+    secret: "asadlfkj!@#!@#dfgasdg",
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore(),
+  })
+);
 
-app.get('*', function(request, response, next){
-  fs.readdir('./data', function(error, filelist){
+// passport는 session을 활성화 시킨 코드 다음에 등장해야한다.
+var passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy;
+
+app.get("*", function (request, response, next) {
+  fs.readdir("./data", function (error, filelist) {
     request.list = filelist;
     next();
   });
 });
 
-var indexRouter = require('./routes/index');
-var topicRouter = require('./routes/topic');
-var authRouter = require('./routes/auth');
+var indexRouter = require("./routes/index");
+var topicRouter = require("./routes/topic");
+var authRouter = require("./routes/auth");
 
-app.use('/', indexRouter);
-app.use('/topic', topicRouter);
-app.use('/auth', authRouter);
+app.use("/", indexRouter);
+app.use("/topic", topicRouter);
+app.use("/auth", authRouter);
 
-app.use(function(req, res, next) {
-  res.status(404).send('Sorry cant find that!');
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry cant find that!");
 });
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
 });
