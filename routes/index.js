6a81f751dae65b4eea896ea9041da9e770f106ef
wyjeon <wyjeon.info@gmail.com>
2021-02-01 14:39:20 +0900

    5.1. passport 세션이용 1

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var template = require('../lib/template.js');
-var auth = require('../lib/auth');
+var template = require("../lib/template.js");
+var auth = require("../lib/auth");
 
-router.get('/', function (request, response) {
-  var title = 'Welcome';
-  var description = 'Hello, Node.js';
+router.get("/", function (request, response) {
+  console.log("/", request.user);
+  var title = "Welcome";
+  var description = "Hello, Node.js";
   var list = template.list(request.list);
-  var html = template.HTML(title, list,
+  var html = template.HTML(
+    title,
+    list,
     `
       <h2>${title}</h2>${description}
       <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
